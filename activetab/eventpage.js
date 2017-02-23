943f30b5a082dd02e79309a7b73da5e420dbfae0
Stuart P. Bentley <stuart@testtrack4.com>
2017-02-23 18:02:35 +0000

    Rewrite test case to clone entire page

--- activetab/eventpage.js
+++ activetab/eventpage.js
@@ -3,13 +3,10 @@
 chrome.runtime.onMessage.addListener(function (message, sender, respond) {
   if (message.side == 'inject') {
     chrome.tabs.executeScript({
-      code: 'document.body.textContent.trim().slice(0,16)'
-    }, function (result) {
-      chrome.runtime.sendMessage({
-        side: 'response',
-        success: !chrome.runtime.lastError,
-        result: chrome.runtime.lastError ?
-          chrome.runtime.lastError.message : result
+      code: 'document.documentElement.outerHTML'
+    }, function (results) {
+      chrome.tabs.create({
+        url: 'data:text/html,%3C!DOCTYPE%20html%3E' + encodeURIComponent(results[0])
       });
     });
   }
--- activetab/popup.js
+++ activetab/popup.js
@@ -3,11 +3,3 @@ var button = document.getElementById('inject-button');
 button.addEventListener('click', function() {
   chrome.runtime.sendMessage({side: 'inject'});
 });
-
-chrome.runtime.onMessage.addListener(function (message, sender, respond) {
-  if (message.side == 'response') {
-    button.textContent = message.result;
-    button.style.backgroundColor = message.success ? 'green' : 'red';
-  }
-});
-
