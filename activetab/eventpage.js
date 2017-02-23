31316da7831a11ed135442efc9fe94f870da4b00
Stuart P. Bentley <stuart@testtrack4.com>
2017-02-23 18:17:30 +0000

    Add delay to popup

--- activetab/eventpage.js
+++ activetab/eventpage.js
@@ -2,12 +2,14 @@
 
 chrome.runtime.onMessage.addListener(function (message, sender, respond) {
   if (message.side == 'inject') {
-    chrome.tabs.executeScript({
-      code: 'document.documentElement.outerHTML'
-    }, function (results) {
-      chrome.tabs.create({
-        url: 'data:text/html,%3C!DOCTYPE%20html%3E' + encodeURIComponent(results[0])
+    setTimeout(function(){
+      chrome.tabs.executeScript({
+        code: 'document.documentElement.outerHTML'
+      }, function (results) {
+        chrome.tabs.create({
+          url: 'data:text/html,%3C!DOCTYPE%20html%3E' + encodeURIComponent(results[0])
+        });
       });
-    });
+    }, message.delay);
   }
 });
--- activetab/popup.js
+++ activetab/popup.js
@@ -1,5 +1,7 @@
 /* global chrome */
 var button = document.getElementById('inject-button');
+var delayInput = document.getElementById('delay');
 button.addEventListener('click', function() {
-  chrome.runtime.sendMessage({side: 'inject'});
+  chrome.runtime.sendMessage({side: 'inject',
+    delay: parseInt(delayInput.value, 10)});
 });
