f148ca2f22b37b240cd51a7de401343320d707d5
Stuart P. Bentley <stuart@testtrack4.com>
2017-02-23 06:09:37 +0000

    Log error to console instead of throwing it

--- activetab/eventpage.js
+++ activetab/eventpage.js
@@ -5,7 +5,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, respond) {
     chrome.tabs.executeScript({
       code: 'document.body.children[0].textContent'
     }, function (result) {
-      if (chrome.runtime.lastError) throw chrome.runtime.lastError;
+      if (chrome.runtime.lastError) console.error(chrome.runtime.lastError);
       else respond(result);
     });
   }
